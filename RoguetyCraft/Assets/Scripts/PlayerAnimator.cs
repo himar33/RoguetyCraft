fa46fa7b92c4183f3a2464a84937f9ab5ee4042a
himarsus <himar.bravo@estudiantat.upc.edu>
2023-05-02 19:14:37 +0200

    Player animator script updated with animations selection in inspector

--- RoguetyCraft/Assets/Scripts/PlayerAnimator.cs
+++ RoguetyCraft/Assets/Scripts/PlayerAnimator.cs
@@ -3,19 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RoguetyCraft.Player.Demo
+namespace RoguetyCraft.Player.Animation
 {
+    public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>
+    {
+        public AnimationClipOverrides(int capacity) : base(capacity) { }
+
+        public AnimationClip this[string name]
+        {
+            get { return this.Find(x => x.Key.name.Equals(name)).Value; }
+            set
+            {
+                int index = this.FindIndex(x => x.Key.name.Equals(name));
+                if (index != -1)
+                    this[index] = new KeyValuePair<AnimationClip, AnimationClip>(this[index].Key, value);
+            }
+        }
+    }
+
+    [RequireComponent(typeof(SpriteRenderer)), RequireComponent(typeof(Animator))]
     public class PlayerAnimator : MonoBehaviour
     {
+        [SerializeField] private AnimationClip _idleClip;
+        [SerializeField] private AnimationClip _runningClip;
+        [SerializeField] private AnimationClip _jumpClip;
+        [SerializeField] private AnimationClip _fallClip;
+
         private SpriteRenderer _sprite;
-        private Animator _animator;
         private PlayerController _playerController;
+
+        private Animator _animator;
+        private AnimatorOverrideController _animatorOverride;
+        private AnimationClipOverrides _animatorClips;
 
         private void Awake()
         {
             _sprite = GetComponent<SpriteRenderer>();
-            _animator = GetComponent<Animator>();
             _playerController = GetComponentInParent<PlayerController>();
+
+            _animator = GetComponent<Animator>();
+            _animatorOverride = new AnimatorOverrideController(_animator.runtimeAnimatorController);
+            _animator.runtimeAnimatorController = _animatorOverride;
+
+            _animatorClips = new AnimationClipOverrides(_animatorOverride.overridesCount);
+            _animatorOverride.GetOverrides(_animatorClips);
+
+            _animatorClips["player_idle_anim"] = _idleClip;
+            _animatorClips["player_run_anim"] = _runningClip;
+            _animatorClips["player_jump_anim"] = _jumpClip;
+            _animatorClips["player_fall_anim"] = _fallClip;
+            _animatorOverride.ApplyOverrides(_animatorClips);
         }
 
         private void Update()
